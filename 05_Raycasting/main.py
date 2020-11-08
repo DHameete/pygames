ab72857a9e72d9a00e73ecde346eff04b375f2e5
Danny Hameeteman <danny.hameeteman@tmc.nl>
2020-11-08 22:07:30 +0100

    one ray casted on wall

--- 05_Raycasting/main.py
+++ 05_Raycasting/main.py
@@ -1,10 +1,11 @@
 import pygame
 from pygame.locals import *
-import sys
+import sys, math
 
 width = 800
 height = 600
 
+WHITE = (255, 255, 255)
 
 class Vec2D:
 
@@ -12,21 +13,69 @@ class Vec2D:
         self.x = x
         self.y = y
         
+    def scale(self, s):
+        self.x = self.x * s
+        self.y = self.y * s
+
+    def normalize(self):
+        n = math.sqrt(self.x * self.x + self.y * self.y)
+        self.x = self.x / n
+        self.y = self.y / n
+
 
 class Ray:
 
-    def __init__(self):
-            pass
+    def __init__(self, x, y):
+        self.pos = Vec2D(x, y)
+        self.dir = Vec2D(1, 0)
+        # self.dir.scale(10)
 
+    def lookAt(self, x, y):
+        self.dir.x = x - self.pos.x
+        self.dir.y = y - self.pos.y
+        self.dir.normalize()
 
-class Boundary:
+    def show(self, surface):
+        start = (self.pos.x, self.pos.y)
+        end = (self.pos.x + 10*self.dir.x, self.pos.y + 10* self.dir.y)
+        pygame.draw.aaline(surface, WHITE, start, end, 1)
+
+    def cast(self, wall):
+        x1 = wall.a.x
+        y1 = wall.a.y
+        x2 = wall.b.x
+        y2 = wall.b.y
+
+        x3 = self.pos.x
+        y3 = self.pos.y
+        x4 = self.pos.x + self.dir.x
+        y4 = self.pos.y + self.dir.y
+
+        den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+        if(den == 0):
+            return
+
+        t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
+        u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den
+
+        if (t > 0 and t < 1 and u > 0):
+            pt = Vec2D(0,0)
+            pt.x = x1 + t * (x2 - x1)
+            pt.y = y1 + t * (y2 - y1)
+            return pt
+        else:
+            return
+
+class Wall:
 
     def __init__(self, x1, y1, x2, y2):
         self.a = Vec2D(x1, y1)
         self.b = Vec2D(x2, y2)
 
     def show(self, surface):
-        pygame.draw.line(surface, (255, 255, 255), (self.a.x, self.a.y), (self.b.x, self.b.y), 1)
+        start = (self.a.x, self.a.y)
+        end = (self.b.x, self.b.y)
+        pygame.draw.aaline(surface, WHITE, start, end, 1)
 
 
 def main():
@@ -36,8 +85,10 @@ def main():
     display = (width, height)
     displaysurface = pygame.display.set_mode(display)
     pygame.display.set_caption("Raycasting")
-    B = Boundary(100,200,150,400)
 
+
+    wall = Wall(500,200,650,400)
+    ray = Ray(100, 300)
 
     # draw
     while True:
@@ -47,7 +98,16 @@ def main():
                 sys.exit()
 
         displaysurface.fill((51, 51, 51))
-        B.show(displaysurface)
+        wall.show(displaysurface)
+        ray.show(displaysurface)
+
+        (mouseX, mouseY) = pygame.mouse.get_pos()
+
+        ray.lookAt(mouseX, mouseY)
+
+        pt = ray.cast(wall)
+        if(pt):
+            pygame.draw.circle(displaysurface, (255, 255, 255), (pt.x, pt.y), 4)
 
         pygame.display.update()
 
