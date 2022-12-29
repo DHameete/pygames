802c346b029672f66164f3ddef3fa23d0ba277dd
Danny Hameeteman <dannyhameeteman@gmail.com>
2022-12-29 21:44:09 +0100

    add tiles, grid and able to draw collapsed tiles

--- 13_WaveFunctionCollapse/main.py
+++ 13_WaveFunctionCollapse/main.py
@@ -4,6 +4,8 @@ import sys, time, math, random
 
 from settings import *
 
+DIM = 4
+
 def main():
 
     # setup
@@ -23,6 +25,17 @@ def main():
     font = pygame.font.SysFont('Arial', 36)
     font.set_bold(True)
 
+    #  Load images
+    tiles = {
+        "BLANK": pygame.image.load("source/blank.png").convert_alpha(),
+        "UP": pygame.image.load("source/up.png").convert_alpha(),
+        "RIGHT": pygame.image.load("source/right.png").convert_alpha(),
+        "DOWN": pygame.image.load("source/down.png").convert_alpha(),
+        "LEFT": pygame.image.load("source/left.png").convert_alpha()
+    }
+
+    grid = [{"collapse": False, "options": ["BLANK","UP","RIGHT","DOWN", "LEFT"], "index": idx} for idx in range(DIM*DIM)]
+
     # loop
     while True:
         for event in pygame.event.get():
@@ -34,6 +47,14 @@ def main():
         background_surface.set_alpha(100) 
         background_surface.fill(DARKGRAY)
         displaysurface.blit(background_surface, (0,0))
+
+        for cell in grid:
+            if cell["collapse"]:
+                idx = cell["index"]
+                y = (idx // DIM) * 50
+                x = (idx % DIM) * 50
+                displaysurface.blit(tiles[cell["options"]], (x,y))
+                # print(idx)
 
 
         # Update display
--- 13_WaveFunctionCollapse/settings.py
+++ 13_WaveFunctionCollapse/settings.py
@@ -1,4 +1,4 @@
-WIDTH = 600
+WIDTH = 400
 HEIGHT = 400
 
 FPS = 60
