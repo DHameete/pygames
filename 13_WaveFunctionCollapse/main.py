865a7ca0b7eeb1d76d71014d8d8e2fa580945933
Danny Hameeteman <dannyhameeteman@gmail.com>
2023-01-01 16:01:12 +0100

    Working Wave Function Collapse

--- 13_WaveFunctionCollapse/main.py
+++ 13_WaveFunctionCollapse/main.py
@@ -4,7 +4,15 @@ import sys, time, math, random
 
 from settings import *
 
-DIM = 8
+DIM = 16
+
+def checkValid(arr, valid):
+    for option in arr[::-1]:
+        if(option in valid):
+            continue
+        else:
+            arr.remove(option)
+
 
 def main():
 
@@ -35,7 +43,41 @@ def main():
     }
 
     # Create empty grid
-    grid = [{"collapse": False, "options": ["BLANK","UP","RIGHT","DOWN", "LEFT"], "index": idx} for idx in range(DIM*DIM)]
+    grid = [{"collapse": False, "options": ["BLANK","UP","RIGHT","DOWN","LEFT"], "pos": (x, y)} for y in range(DIM) for x in range(DIM)]
+
+    rules = {
+        "BLANK": {
+            "UP": ["BLANK","UP"],
+            "RIGHT": ["BLANK","RIGHT"],
+            "DOWN": ["BLANK","DOWN"],
+            "LEFT": ["BLANK","LEFT"]
+        },
+        "UP": {
+            "UP": ["RIGHT","DOWN","LEFT"],
+            "RIGHT": ["UP","DOWN","LEFT"],
+            "DOWN": ["BLANK","DOWN"],
+            "LEFT": ["UP","RIGHT","DOWN"]
+        },
+        "RIGHT": {
+            "UP": ["RIGHT","DOWN","LEFT"],
+            "RIGHT": ["UP","DOWN","LEFT"],
+            "DOWN": ["UP","RIGHT","LEFT"],
+            "LEFT": ["BLANK","LEFT"]
+        },
+        "DOWN": {
+            "UP": ["BLANK","UP"],
+            "RIGHT": ["UP","DOWN","LEFT"],
+            "DOWN": ["UP","RIGHT","LEFT"],
+            "LEFT": ["UP","RIGHT","DOWN"]
+        },
+        "LEFT": {
+            "UP": ["RIGHT","DOWN","LEFT"],
+            "RIGHT": ["BLANK","RIGHT"],
+            "DOWN": ["UP","RIGHT","LEFT"],
+            "LEFT": ["UP","RIGHT","DOWN"]
+        }
+    }
+    # print(grid)
 
     # loop
     while True:
@@ -52,7 +94,8 @@ def main():
         # Pick cell with lowest entropy
         gridCopy = grid[:]
         gridCopy.sort(key=lambda cell: len(cell["options"]))
-        gridFiltered = list(filter(lambda cell: len(cell["options"])==len(gridCopy[0]["options"]), gridCopy))
+        gridUncollapsed = list(filter(lambda cell: cell["collapse"] == False, gridCopy))
+        gridFiltered = list(filter(lambda cell: len(cell["options"])==len(gridUncollapsed[0]["options"]), gridUncollapsed))
 
         if (gridFiltered):
             cell = random.choice(gridFiltered)
@@ -60,14 +103,39 @@ def main():
             pick = random.choice(cell["options"])
             cell["options"] = [pick]
 
+        # Next tiles
+        (ix, iy) = cell["pos"]
+        # Look up
+        if (iy > 0):
+            cell_next = grid[ix + (iy-1) * DIM]["options"]
+            valid = rules[cell["options"][0]]["UP"]
+            checkValid(cell_next, valid)
+        # Look right
+        if (ix < DIM-1):
+            cell_next = grid[ix+1 + (iy) * DIM]["options"]
+            valid = rules[cell["options"][0]]["RIGHT"]
+            checkValid(cell_next, valid)
+        # Look down
+        if (iy < DIM-1):
+            cell_next = grid[ix + (iy+1) * DIM]["options"]
+            valid = rules[cell["options"][0]]["DOWN"]
+            checkValid(cell_next, valid)
+        # Look left
+        if (ix > 0):
+            cell_next = grid[ix-1 + (iy) * DIM]["options"]
+            valid = rules[cell["options"][0]]["LEFT"]
+            checkValid(cell_next, valid)
+
         # Draw grid
         for cell in grid:
+            (x, y) = tuple([z * 50 for z in cell["pos"]])
             if cell["collapse"]:
-                idx = cell["index"]
-                y = (idx // DIM) * 50
-                x = (idx % DIM) * 50
                 displaysurface.blit(tiles[cell["options"][0]], (x,y))
-
+            else:
+                # text = font.render(f'{len(cell["options"])}', True, WHITE)
+                # text_rect = text.get_rect(center=(x+25,y+25))
+                # displaysurface.blit(text,text_rect)
+                pass
 
         # Update display
         pygame.display.update()
